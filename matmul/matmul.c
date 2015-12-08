05e334672960628cd401b1d425b986dffedbccfc
HerrAndersson <anderssonjonas94@gmail.com>
2015-12-08 16:42:39 +0100

    Changed to tab instead of 4 spaces and cleanup

--- matmul/matmul.c
+++ matmul/matmul.c
@@ -6,60 +6,67 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+//#include <mpi.h>
 
-#define SIZE 1024
+#define SIZE 8
 
 static double a[SIZE][SIZE];
 static double b[SIZE][SIZE];
 static double c[SIZE][SIZE];
 
-static void
-init_matrix(void)
+static void init_matrix(void)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < SIZE; i++)
-        for (j = 0; j < SIZE; j++) {
-	    /* Simple initialization, which enables us to easy check
-	     * the correct answer. Each element in c will have the same 
-	     * value as SIZE after the matmul operation.
-	     */
-	    a[i][j] = 1.0;
-	    b[i][j] = 1.0;
-        }
+	for (i = 0; i < SIZE; i++)
+	{
+		for (j = 0; j < SIZE; j++) 
+		{
+			/* Simple initialization, which enables us to easy check
+			* the correct answer. Each element in c will have the same 
+			* value as SIZE after the matmul operation. */
+			
+			a[i][j] = 1.0;
+			b[i][j] = 1.0;
+		}
+	}
 }
 
-static void
-matmul_seq()
+static void matmul_seq()
 {
-    int i, j, k;
+	int i, j, k;
 
-    for (i = 0; i < SIZE; i++) {
-        for (j = 0; j < SIZE; j++) {
-            c[i][j] = 0.0;
-            for (k = 0; k < SIZE; k++)
-                c[i][j] = c[i][j] + a[i][k] * b[k][j];
-        }
-    }
+	for (i = 0; i < SIZE; i++) 
+	{
+		for (j = 0; j < SIZE; j++) 
+		{
+			c[i][j] = 0.0;
+			for (k = 0; k < SIZE; k++)
+				c[i][j] = c[i][j] + a[i][k] * b[k][j];
+		}
+	}
 }
 
-static void
-print_matrix(void)
+static void print_matrix(void)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < SIZE; i++) {
-        for (j = 0; j < SIZE; j++)
-	    printf(" %7.2f", c[i][j]);
-	printf("\n");
-    }
+	for (i = 0; i < SIZE; i++) 
+	{
+		for (j = 0; j < SIZE; j++)
+		printf(" %7.2f", c[i][j]);
+		printf("\n");
+	}
 }
 
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-    init_matrix();
-    matmul_seq();
-    //print_matrix();
+	//MPI_Status stat;
+	pthread_t threads[8];
+	
+	init_matrix();
+	matmul_seq();
+	print_matrix();
 }
 
