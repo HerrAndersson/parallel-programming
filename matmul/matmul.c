77c1a024e75079450c4bd6fc58077b399f383266
HerrAndersson <anderssonjonas94@gmail.com>
2015-12-10 17:27:32 +0100

    Matmul is now working correctly

--- matmul/matmul.c
+++ matmul/matmul.c
@@ -8,8 +8,8 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-//#define SIZE 1024
-#define SIZE 4
+#define SIZE 1024
+//#define SIZE 4
 
 static double a[SIZE][SIZE];
 static double b[SIZE][SIZE];
@@ -40,8 +40,8 @@ static void init_matrix(void)
 			* the correct answer. Each element in c will have the same 
 			* value as SIZE after the matmul operation. */
 			
-			a[i][j] = rand() % 10;
-			b[i][j] = rand() % 10;
+			a[i][j] = 1.0;
+			b[i][j] = 1.0;
 			c[i][j] = 0.0;
 		}
 	}
@@ -51,9 +51,31 @@ static void* matmul(void* arg)
 {
 	int i, j, k;
 	
-	Data* data = (Data*)arg;
+	Data* d = (Data*)arg;
+	int aStartX = d->aStartX;
+	int aStartY = d->aStartY;
+	int bStartX = d->bStartX;
+	int bStartY = d->bStartY;
+	int cStartX = d->cStartX;
+	int cStartY = d->cStartY;
+	int length 	= d->length;
 	
-	printf("%d %d %d %d %d %d %d\n", data->aStartX, data->aStartY, data->bStartX, data->bStartY, data->cStartX, data->cStartY, data->length);
+	int cEndY = cStartY + length;
+	int cEndX = cStartX + length;
+	
+	for (i = cStartY; i < cEndY; i++)
+	{
+		for (j = cStartX; j < cEndX; j++)
+		{
+			for (k = 0; k < length; k++)
+			{
+				c[i][j] += a[i][aStartX+k] * b[bStartY+k][j];
+			}
+		}
+	}
+	
+	
+/*	printf("%d %d %d %d %d %d %d\n", data->aStartX, data->aStartY, data->bStartX, data->bStartY, data->cStartX, data->cStartY, data->length);
 	
 	int l = data->cStartY;
 	int m = data->cStartX;
@@ -66,22 +88,27 @@ static void* matmul(void* arg)
 			{
 				pthread_mutex_lock(&lock);
 				
-				c[m][l] += a[i][data->aStartY + k] * b[data->bStartX + k][j];
-	
+				
+				c[data->cStartX+k][l] += a[i][data->aStartY + k] * b[data->bStartX + k][j];
+				printf("Inside inner loop, %f %f %f\n", a[i][data->aStartY + k], b[data->bStartX + k][j], c[data->cStartX+k][l]);
 				pthread_mutex_unlock(&lock);
 			}
 			
 			l += 1;
 		}
 		m += 1;
-	}
+	}*/
+	
+	
+	
+	
 }
 
 static void print_matrix(void)
 {
 	int i, j;
 
-	for (i = 0; i < SIZE; i++) 
+	/*for (i = 0; i < SIZE; i++) 
 	{
 		for (j = 0; j < SIZE; j++)
 			printf(" %7.2f", a[i][j]);
@@ -108,6 +135,7 @@ static void print_matrix(void)
 			
 		printf("\n");
 	}
+	*/
 }
 
 int main(int argc, char **argv)
@@ -116,7 +144,7 @@ int main(int argc, char **argv)
 	pthread_t threads[8];
 	init_matrix();
 	
-	//a1*b1
+	//a1*a2 -> c00
 	Data d;
 	d.aStartX = 0;
 	d.aStartY = 0;
@@ -130,96 +158,96 @@ int main(int argc, char **argv)
 	
 	pthread_create(&(threads[0]), NULL, matmul, &d);
 	
-	//a2*b2
+	//b1*c2 -> c00
 	Data d1;
 	d1.aStartX = SIZE / 2;
 	d1.aStartY = 0;
-	d1.bStartX = SIZE / 2;
-	d1.bStartY = 0;
+	d1.bStartX = 0;
+	d1.bStartY = SIZE / 2;
 	
-	d1.cStartX = SIZE / 2;
+	d1.cStartX = 0;
 	d1.cStartY = 0;
 	
 	d1.length = SIZE / 2;
 
 	pthread_create(&(threads[1]), NULL, matmul, &d1);
 	
-	//a3*b3
+	//a1*b2 ->c10
 	Data d2;
 	d2.aStartX = 0;
-	d2.aStartY = SIZE / 2;
-	d2.bStartX = 0;
-	d2.bStartY = SIZE / 2;
+	d2.aStartY = 0;
+	d2.bStartX = SIZE / 2;
+	d2.bStartY = 0;
 	
-	d2.cStartX = 0;
-	d2.cStartY = SIZE / 2;
+	d2.cStartX = SIZE / 2;
+	d2.cStartY = 0;
 	
 	d2.length = SIZE / 2;
 
 	pthread_create(&(threads[2]), NULL, matmul, &d2);
 	
-	//a4*b4
+	//b1*d2 -> c10
 	Data d3;
 	d3.aStartX = SIZE / 2;
-	d3.aStartY = SIZE / 2;
+	d3.aStartY = 0;
 	d3.bStartX = SIZE / 2;
 	d3.bStartY = SIZE / 2;
 	
 	d3.cStartX = SIZE / 2;
-	d3.cStartY = SIZE / 2;
+	d3.cStartY = 0;
 	
 	d3.length = SIZE / 2;
 
 	pthread_create(&(threads[3]), NULL, matmul, &d3);
 	
-	//a2*b3 -> c00
+	//c1*a2 -> c01
 	Data d4;
-	d4.aStartX = SIZE / 2;
-	d4.aStartY = 0;
+	d4.aStartX = 0;
+	d4.aStartY = SIZE / 2;
 	d4.bStartX = 0;
-	d4.bStartY = SIZE / 2;
+	d4.bStartY = 0;
 	
 	d4.cStartX = 0;
-	d4.cStartY = 0;
+	d4.cStartY = SIZE / 2;
 	
 	d4.length = SIZE / 2;
 
 	pthread_create(&(threads[4]), NULL, matmul, &d4);
 	
-	//a1*b4 -> c01
+	//d1*c2 -> c01
 	Data d5;
-	d5.aStartX = 0;
-	d5.aStartY = 0;
-	d5.bStartX = SIZE / 2;
+	d5.aStartX = SIZE / 2;
+	d5.aStartY = SIZE / 2;
+	d5.bStartX = 0;
 	d5.bStartY = SIZE / 2;
 	
-	d5.cStartX = SIZE / 2;
-	d5.cStartY = 0;
+	d5.cStartX = 0;
+	d5.cStartY = SIZE / 2;
 	
 	d5.length = SIZE / 2;
 
 	pthread_create(&(threads[5]), NULL, matmul, &d5);
 	
-	//a4*b1 -> c10
+	//c1*b2 -> c11
 	Data d6;
-	d6.aStartX = SIZE / 2;
+	d6.aStartX = 0;
 	d6.aStartY = SIZE / 2;
-	d6.bStartX = 0;
+	d6.bStartX = SIZE / 2;
 	d6.bStartY = 0;
 	
-	d6.cStartX = 0;
+	d6.cStartX = SIZE / 2;
 	d6.cStartY = SIZE / 2;
 	
 	d6.length = SIZE / 2;
 
 	pthread_create(&(threads[6]), NULL, matmul, &d6);
 	
-	//a3*b2 -> c11
+	//d1*d2 -> c11
 	Data d7;
-	d7.aStartX = 0;
+	d7.aStartX = SIZE / 2;
 	d7.aStartY = SIZE / 2;
 	d7.bStartX = SIZE / 2;
-	d7.bStartY = 0;
+	d7.bStartY = SIZE / 2;
 	
 	d7.cStartX = SIZE / 2;
 	d7.cStartY = SIZE / 2;
@@ -234,6 +262,6 @@ int main(int argc, char **argv)
 		pthread_join(threads[i], NULL);
 	}
 	
-	print_matrix();
+	//print_matrix();
 }
 
