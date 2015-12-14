79f874d3963ccdd530cc85d7026071e7b932bb7b
HerrAndersson <anderssonjonas94@gmail.com>
2015-12-14 17:15:04 +0100

    Implemented threaded qsort with the SAME speed as single thread!

--- qsort/qsort.c
+++ qsort/qsort.c
@@ -8,15 +8,15 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define KILO (2)
+#define KILO (256)
 //#define KILO (1024)
 
 #define MEGA (KILO*KILO)
 
 //#define MAX_ITEMS (64*MEGA)
-#define MAX_ITEMS (4*MEGA)
+#define MAX_ITEMS (64*MEGA)
 
-#define NUM_THREADS 8
+#define NUM_THREADS 16
 
 #define swap(v, a, b) {unsigned tmp; tmp=v[a]; v[a]=v[b]; v[b]=tmp;}
 
@@ -139,50 +139,84 @@ static void* quick_sort(void* arg)
 
 	/* select the pivot value */
 	pivot_index = (low+high)/2;
-
 	/* partition the vector */
 	pivot_index = partition(v, low, high, pivot_index);
 
+	pthread_t threads[2];
+	int r1 = 0;
+	int r2 = 0;
+	int isSeq = ((high-low) < MEGA);
+	//printf("%d %d\n",high-low,  isSeq);
 	/* sort the two sub arrays */
 	if (low < pivot_index)
 	{
-	 	seq_quick_sort(v, low, pivot_index-1);
+		if(isSeq)
+		{
+			seq_quick_sort(v, low, pivot_index-1);
+		}
+		else
+		{	
+			r1 = 1;
+			Data d;
+			d.v = v;
+			d.low = low;
+			d.high = pivot_index-1;
+		
+			pthread_create(&(threads[0]), NULL, quick_sort, &d);
+			//printf("Create thread\n");
+		}
 	}
 	
 	if (pivot_index < high)
 	{	
-		seq_quick_sort(v, pivot_index+1, high);
+		if(isSeq)
+		{
+			seq_quick_sort(v, pivot_index+1, high);
+		}
+		else
+		{
+			r2 = 1;
+			Data d;
+			d.v = v;
+			d.low = pivot_index+1;
+			d.high = high;
+		
+			pthread_create(&(threads[1]), NULL, quick_sort, &d);
+			//printf("Create thread\n");
+		}
 	}	
+	
+	if(r1 == 1)
+	{
+		pthread_join(threads[0], NULL);
+	}
+	
+	if(r2 == 1)
+	{
+		pthread_join(threads[1], NULL);
+	}
 }
 
 int main(int argc, char **argv)
 {
 	srand(time(0));
 	init_array();
-	print_array();
+	//print_array();
 	
-	pthread_t threads[NUM_THREADS];
-	Data d[NUM_THREADS];
 	
-	int i = 0;
-	for (i = 0; i < NUM_THREADS; i++)
-	{
-		d[i].v = v;	
-		d[i].low = i * (MAX_ITEMS / NUM_THREADS);
-		d[i].high = i * (MAX_ITEMS / NUM_THREADS) + (MAX_ITEMS / NUM_THREADS) - 1;
+	/*Data d;
+	d.v = v;
+	d.low = 0;
+	d.high = MAX_ITEMS - 1;
+	*/
+	
+	//quick_sort(&d);
 
-		pthread_create(&(threads[i]), NULL, quick_sort, &d[i]);
-	}
 	
-	for (i = 0; i < NUM_THREADS; i++)
-	{
-		pthread_join(threads[i], NULL);
-	}
-	
-	print_array();
+	//print_array();
 	
 	seq_quick_sort(v, 0, MAX_ITEMS-1);
 	
-	print_array();
+	//print_array();
 }
 
